734ff3a627f2b53b3947569424dd1337619cf7a2
Petrisor Lacatus <petrisor.lacatus@insro.ro>
2015-03-02 15:17:01 +0200

    Added initialized project

---
+++ src/main/java/sapd/SapdApplication.java
@@ -0,0 +1,12 @@
+package sapd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SapdApplication.class, args);
+    }
+}
