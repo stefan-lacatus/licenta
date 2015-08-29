c8d87779e95083f31f6c2663ff872921277f6ed8
unknown <placatus@PLACATUS1L.ptcnet.ptc.com>
2015-08-29 17:40:35 +0300

    Added logging

--- src/main/java/ro/pub/acse/sapd/controller/web/monitoring/LogsController.java
+++ src/main/java/ro/pub/acse/sapd/controller/web/monitoring/LogsController.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Displays the logs in a webpage
+ */
 @Controller
 @RequestMapping(value = "/monitor")
-public class DataChannelCharts {
-
-    @RequestMapping(value = {"channels", "channels/"})
+public class LogsController {
+    @RequestMapping(value = {"logs", "logs/"})
     public String managementHome(Model model) {
         model.addAttribute("current_page", "monitor");
-        return "monitoring/channel";
+        return "monitoring/logs";
     }
 }
---
+++ src/main/java/ro/pub/acse/sapd/controller/web/monitoring/MonitorChannelController.java
@@ -0,0 +1,16 @@
+package ro.pub.acse.sapd.controller.web.monitoring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/monitor")
+public class MonitorChannelController {
+
+    @RequestMapping(value = {"channels", "channels/"})
+    public String managementHome(Model model) {
+        model.addAttribute("current_page", "monitor");
+        return "monitoring/channel";
+    }
+}
