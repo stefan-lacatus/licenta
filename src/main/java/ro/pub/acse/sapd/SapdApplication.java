246e692c7a124776d29c945756a0252a3856f5aa
unknown <placatus@PLACATUS1L.ptcnet.ptc.com>
2015-09-06 21:09:25 +0300

    try to make heroku work

--- src/main/java/ro/pub/acse/sapd/SapdApplication.java
+++ src/main/java/ro/pub/acse/sapd/SapdApplication.java
@@ -1,13 +1,11 @@
 package ro.pub.acse.sapd;
 
-import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-@EnableJSONDoc
 public class SapdApplication {
 
     public static void main(String[] args) {
